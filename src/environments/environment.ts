bd4327b55a2807d1f69966f74eb00c72ba52feed
JaimeValleZ <jaime.valle@cun.edu.co>
2026-01-22 22:40:16 -0500

    organizar imagenes para usar en render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://scattered-ronny-gofly-flight-48fa32b2.koyeb.app'
+  apiUrl: 'https://written-dacie-gateway-gofly-a6bf2441.koyeb.app'
 };
