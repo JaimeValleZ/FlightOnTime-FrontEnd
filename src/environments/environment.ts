5bac44a0e3fc43e63d58c4ddbdd44697ddcb9c8f
JaimeValleZ <jaime.valle@cun.edu.co>
2026-01-22 22:44:00 -0500

    modificacion en ruta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://written-dacie-gateway-gofly-a6bf2441.koyeb.app'
+  apiUrl: 'https://written-dacie-gateway-gofly-a6bf2441.koyeb.app/'
 };
